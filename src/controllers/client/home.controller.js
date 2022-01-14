da00486ef23a488cef2a29fee043aec7a611c587
dinhquanganh <dinhquanganhdev@gmail.com>
2022-01-14 20:39:07 +0700

    update image

--- src/controllers/client/home.controller.js
+++ src/controllers/client/home.controller.js
@@ -21,46 +21,9 @@ const getDataHome = catchAsync(async (req, res) => {
   }));
 
   res.render('home', {
-    title: 'Danh sách sản phẩm',
+    title: 'Trang chủ',
     tea,
   });
-});
-
-const getProduct = catchAsync(async (req, res) => {
-  if (req?.params?.productId) {
-    const product = await productService.getProductById(req.params.productId);
-
-    res.setHeader(
-      'Content-Security-Policy',
-      "default-src *; script-src * 'unsafe-inline'; img-src * data:; style-src * 'unsafe-inline'; connect-src *; font-src *; object-src *; media-src *; frame-src *; frame-ancestors *; base-uri *; form-action *; block-all-mixed-content; upgrade-insecure-requests;"
-    );
-
-    res.render('product', {
-      title: 'Chỉnh sửa sản phẩm',
-      product,
-      id: product._id.toString(),
-      categoryList: [
-        {
-          name: 'Trà',
-          value: 'tea',
-        },
-        {
-          name: 'Đồ ăn / Gia vị',
-          value: 'food-spice',
-        },
-        {
-          name: 'Hạt / Hoa quả sấy',
-          value: 'seed-fruit',
-        },
-        {
-          name: 'Mật ong / Dược liệu',
-          value: 'honey-medicine',
-        },
-      ],
-    });
-  } else {
-    res.redirect('/admin/product');
-  }
 });
 
 module.exports = {
--- src/controllers/client/product.controller.js
+++ src/controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 const catchAsync = require('../../utils/catchAsync');
 const { productService } = require('../../services');
 const { convertDeltaToHtml } = require('node-quill-converter');
+var QuillDeltaToHtmlConverter =
+  require('quill-delta-to-html').QuillDeltaToHtmlConverter;
 
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
@@ -15,27 +17,42 @@ const getProduct = catchAsync(async (req, res) => {
     let { name, description, _id, images, price, category } =
       await productService.getProductById(req.params.id);
 
+    console.log(name);
+
     res.setHeader(
       'Content-Security-Policy',
       "default-src *; script-src * 'unsafe-inline'; img-src * data:; style-src * 'unsafe-inline'; connect-src *; font-src *; object-src *; media-src *; frame-src *; frame-ancestors *; base-uri *; form-action *; block-all-mixed-content; upgrade-insecure-requests;"
     );
 
-    product = {
+    // let product = {
+    //   name,
+    //   description: new QuillDeltaToHtmlConverter(
+    //     JSON.parse(replaceAll(description, '//endofline**n', `\\n`)),
+    //     {}
+    //   ),
+    //   _id,
+    //   images,
+    //   price,
+    //   category,
+    // };
+
+    let product = {
       name,
-      description: replaceAll(description, '//endofline**n', `\\n`),
+      description: convertDeltaToHtml(
+        JSON.parse(replaceAll(description, '//endofline**n', `\\n`))
+      ),
       _id,
       images,
       price,
       category,
     };
 
-    return res.render('product-detail', {
+    console.log(product);
+
+    res.render('product-detail', {
       title: product.name,
       page: 'product',
-      product: {
-        ...product,
-        description: convertDeltaToHtml(JSON.parse(description)),
-      },
+      product,
       id: req.params.productId,
     });
   }
